52cb71448c126146f6e54c7f09280b181a94c723
Nikita <nikitalubitelc@gmail.com>
2026-02-24 23:37:08 -0800

    commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,12 +1,20 @@
 <?php
 	namespace App\Http\Controllers;
-	
+	use Illuminate\Support\Facades\Route;
+
+	Route::get('/user', [UserController::class, 'show']);
+	Route::get('/user/{name}', [UserController::class, 'show']);
+	Route::get('/user/{surname}/{name}', [UserController::class, 'show']);
+
 	class UserController extends Controller
 	{
 			public function show()
 		{
-			return 'hello world';
+			return 'user';
 		}
+		
 
 	}
+
+
 ?>
