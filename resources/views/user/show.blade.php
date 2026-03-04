23ee13195fa46c52d2dd60d758c50068f9a5d13b
Nikita <nikitalubitelc@gmail.com>
2026-03-03 21:12:12 -0800

    commit

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -3,14 +3,5 @@
 		{{ $title }} - site.com
 	</x-slot>
 	
-    <p>{{ $name }}</p>
-    <p>{{ $age }}</p>
-    <p>{{ $salary }}</p>
-    <a href="{{ $name }}">{{ $name }}</a>
-    <p>{{ $name }}</p>
-	<p><input class="{{ $name }}"></p>
-    <p><input value="{{ $age }}"></p>
-    <p><input value="{{ $salary }}"></p>
-    <p><input style="{{ $name }}">$name</p>
-    current timestamp: {{ time() }}
+    
 </x-layout>
