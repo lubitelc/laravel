4c1b0e87627f67226ef56bf3d086be42419f7e34
Nikita <nikitalubitelc@gmail.com>
2026-03-31 21:21:54 -0700

    coma

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,22 +19,56 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        DB::table('posts')->insert([
+        DB::table('users')->insert([
 				[
-					'title' => 'title 1',
-					'slug'  => 'post-1',
-					'text'  => 'text text text 1',
+					'name' => 'Vova',
+					'email'  => 'Vova@mail.ru',
+					'age'  => '30',
+					'salary'  => '5000',
+					'created_at'  => '2026-02-12',
+					'updated_at'  => '2026-02-13',
 				],
 				[
-					'title' => 'title 2',
-					'slug'  => 'post-2',
-					'text'  => 'text text text 2',
+					'name' => 'Max',
+					'email'  => 'Max@mail.ru',
+					'age'  => '15',
+					'salary'  => '3000',
+					'created_at'  => '2025-12-30',
+					'updated_at'  => '2026-01-01',
 				],
 				[
-					'title' => Str::random(10),
-				    'slug'  => Str::random(10),
-				    'text'  => Str::random(50),
+					'name' => 'Serega',
+					'email'  => 'Serega@mail.ru',
+					'age'  => '18',
+					'salary'  => '15000',
+					'created_at'  => '2025-02-15',
+					'updated_at'  => '2026-02-15',
 				],
+				[
+					'name' => 'Lesha',
+					'email'  => 'Lesha@mail.ru',
+					'age'  => '20',
+					'salary'  => '20000',
+					'created_at'  => '2023-02-12',
+					'updated_at'  => '2025-02-13',
+				],
+				[
+					'name' => 'Vlad',
+					'email'  => 'Vlad@mail.ru',
+					'age'  => '40',
+					'salary'  => '55000',
+					'created_at'  => '2026-01-01',
+					'updated_at'  => '2026-04-01',
+				],
+				[
+					'name' => 'Kirill',
+					'email'  => 'Kirill@mail.ru',
+					'age'  => '35',
+					'salary'  => '125000',
+					'created_at'  => '2020-06-12',
+					'updated_at'  => '2026-03-30',
+				],
+				
 			]);
 
 
